0efbde1fb9f872f2f4fc18393d2d063a85e35438
fangmin <1261869845@qq.com>
2018-01-20 22:08:24 +0800

    fix package

--- src/main/java/com/fangminx/XunwuApplication.java
+++ src/main/java/com/fangminx/XunwuApplication.java
@@ -1,4 +1,4 @@
-package com.fangminx.xunwu;
+package com.fangminx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/fangminx/XunwuApplicationTests.java
+++ src/test/java/com/fangminx/XunwuApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fangminx.xunwu;
+package com.fangminx;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
