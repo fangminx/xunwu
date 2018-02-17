0949c00e5ba1843778f121d78cda9b85a05296fe
fangmin <min.fang@huilianyi.com>
2018-02-18 01:01:03 +0800

    redis session会话存储登录信息

---
+++ src/main/java/com/fangminx/config/RedisSessionConfig.java
@@ -0,0 +1,19 @@
+package com.fangminx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * session会话
+ */
+//@Configuration
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400)
+public class RedisSessionConfig {
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+
+        return new StringRedisTemplate(factory);
+    }
+}
